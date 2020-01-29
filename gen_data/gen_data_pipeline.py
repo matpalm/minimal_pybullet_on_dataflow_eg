e70b9d0e12a1253b89ec48dc4e2543a4f4b5ccdd
Mat Kelcey <matthew.kelcey@gmail.com>
2020-01-29 13:51:06 +1100

    some comments

--- gen_data/gen_data_pipeline.py
+++ gen_data/gen_data_pipeline.py
@@ -28,8 +28,18 @@ def run(argv=None):
     known_args, pipeline_args = parser.parse_known_args(argv)
 
     with beam.Pipeline(argv=pipeline_args) as p:
+
+        # collect seeds; one line per pybullet run
         seeds = p | ReadFromText(known_args.input)
+
+        # stitch additional config next to the seed. this could also
+        # be done as a stateful DoFn
         render_config = seeds | beam.Map(
             lambda s: (s, known_args.output_render_png))
+
+        # run the renders, but with a wrapper that catches and logs
+        # exceptions
         render_infos = render_config | beam.Map(render_logging_exceptions)
+
+        # collect output from renders (json) and write out
         _out = render_infos | WriteToText(known_args.output_render_info)
